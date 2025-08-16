6ef406e9eae137b3c3b5d36fe7c3c4851d6a1a97
Nana Kwame Amponsah <nkwame11604@gmail.com>
2025-08-15 23:55:51 -0400

    CORS configuration

--- audio-transcribe/src/main/java/com/audio/transcribe/WebConfig.java
+++ audio-transcribe/src/main/java/com/audio/transcribe/WebConfig.java
@@ -1,2 +1,18 @@
-package com.audio.transcribe;public class WebConfig {
+package com.audio.transcribe;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig  implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5174")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
